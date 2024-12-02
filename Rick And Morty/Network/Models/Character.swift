dffc52879fdfbcb2efbc6a8cf9c7b1fae451bf50
Matthew Chukwuemeka <martins.twist@gmail.com>
2024-12-02 13:55:17 +0100

    first commit

---
+++ Rick And Morty/Network/Models/Character.swift
@@ -0,0 +1,12 @@
+//
+//  Character.swift
+//  Rick And Morty
+//
+//  Created by Matthew Chukwuemeka on 02/12/2024.
+//
+
+struct Character {
+    let name: String
+    let image: String
+    let species: String
+}
---
+++ Rick And Morty/Network/Models/CharacterDetails.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterDetails.swift
+//  Rick And Morty
+//
+//  Created by Matthew Chukwuemeka on 02/12/2024.
+//
+
+struct CharacterDetails {
+    let name: String
+    let image: String
+    let species: String
+    let status: String
+    let gender: String
+}
