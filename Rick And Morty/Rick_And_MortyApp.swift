4193e9cd0b9f0aa074e447035d16dafd52fc5e57
Matthew Chukwuemeka <martins.twist@gmail.com>
2024-12-02 13:35:45 +0100

    Initial Commit

---
+++ Rick And Morty/Rick_And_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_And_MortyApp.swift
+//  Rick And Morty
+//
+//  Created by Matthew Chukwuemeka on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rick_And_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
