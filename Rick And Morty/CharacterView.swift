136c1f7375bc0b051554692d7bf36a13cc5b5014
Matthew Chukwuemeka <martins.twist@gmail.com>
2024-12-03 13:16:43 +0100

    add character view

---
+++ Rick And Morty/CharacterView.swift
@@ -0,0 +1,71 @@
+//
+//  CharacterView.swift
+//  Rick And Morty
+//
+//  Created by Matthew Chukwuemeka on 03/12/2024.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    var character: Character
+    var body: some View {
+        HStack {
+            AsyncImage(url: URL(string: character.image)) { image in
+                image
+                    .resizable()
+                    .frame(width: 75, height: 75)
+            } placeholder: {
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 75, height: 75)
+                    .background(Color.gray)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+
+            VStack(alignment: .leading) {
+                Text(character.name)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text(character.species)
+                    .opacity(0.8)
+                Spacer()
+            }
+            .padding(.top, 4)
+
+            Spacer()
+        }
+        .padding()
+        .frame(height: 108)
+        .background(alignment: .center) {
+            background(status: character.status)
+        }
+    }
+
+
+    @ViewBuilder
+    func background(status: CharacterStatus) -> some View {
+        switch status {
+        case .alive:
+            Color("alive")
+                .cornerRadius(12)
+        case .dead:
+            Color("dead")
+                .opacity(0.5)
+                .cornerRadius(12)
+
+        case .unknown:
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(.gray, lineWidth: 1)
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        CharacterView(character: .mock(status: .alive))
+        CharacterView(character: .mock(status: .dead))
+        CharacterView(character: .mock(status: .unknown))
+    }
+    .padding(.horizontal)
+}
--- Rick And Morty/Network/Models/Character.swift
+++ Rick And Morty/Network/Models/Character.swift
@@ -5,18 +5,24 @@
 //  Created by Matthew Chukwuemeka on 02/12/2024.
 //
 
-struct Character: Codable {
+import Fakery
+import Foundation
+
+struct Character: Codable, Identifiable {
     let id: Int
     let name: String
     let image: String
     let species: String
+    let status: CharacterStatus
 
-    static func mock() -> Character {
-        Character(
-            id: 1,
-            name: "Rick Sanchez",
-            image: "rick",
-            species: "Human"
+    static func mock(status: CharacterStatus = .alive) -> Character {
+        let faker = Faker()
+        return Character(
+            id: faker.number.randomInt(),
+            name: faker.name.name(),
+            image: faker.internet.image(),
+            species: "Human",
+            status: status
         )
     }
 }
